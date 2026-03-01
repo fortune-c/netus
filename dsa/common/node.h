e332b18722668e67f5336ba863b68f36f06dbddf
fortune-c <fortunate.adesinas@gmail.com>
2026-03-01 09:39:29 +0100

    http bug fixe

---
+++ dsa/common/node.h
@@ -0,0 +1,44 @@
+//
+// Created by fortune on 3/1/26.
+//
+
+/*
+ The Node struct is the foundation for list objects, functioning like a single cell in the list.
+ Nodes utilize void pointers to store data, and as such are capable of storing any data type.
+
+ Typically, nodes should not be manually instantiated.
+ Usage is typically handled by the various list objects.
+
+ In order to work, this requires the data to be allocated on the heap prior to being inserted into a node object.
+ Typically, this is handled automatically when the node constructor is used;
+ however, custom data types (those not included in the enumeration) need to be allocated manually and the data type Special should be used.
+
+ In order to destroy a node, the node destructor is recommended as it will free both the stored data and the node itself automatically.
+ */
+
+
+#ifndef WEB_SERVER_NODE_H
+#define WEB_SERVER_NODE_H
+
+// MARK: DATA TYPES
+
+// Nodes are used to store data of any type in a list.
+struct Node
+{
+    /* PUBLIC MEMBER VARIABLES */
+    // The data is stored as a void pointer - casting is required for proper access.
+    void *data;
+    // A pointer to the next node in the chain.
+    struct Node *next;
+    struct Node *previous;
+};
+
+
+// MARK: CONSTRUCTORS
+
+// The constructor should be used to create nodes.
+struct Node node_constructor(void *data, unsigned long size);
+// The destructor should be used to destroy nodes.
+void node_destructor(struct Node *node);
+
+#endif //WEB_SERVER_NODE_H
---
+++ dsa/lists/queue.h
@@ -0,0 +1,45 @@
+//
+// Created by fortune on 3/1/26.
+//
+
+/*
+ The Queue struct is a version of the LinkedList the enforces rules on how items are added and accessed.
+ In short, items are always added to the end and removed from the front.
+ Largely, the methods defined here simply utilize those from LinkedList in a predefined way.
+ */
+
+
+#ifndef WEB_SERVER_QUEUE_H
+#define WEB_SERVER_QUEUE_H
+
+#include "lin.h"
+
+
+// MARK: DATA TYPES
+
+// Queues are used to access a linked list in a controlled manner.
+struct Queue
+{
+    /* MEMBER VARIABLES */
+    // A reference to the embedded LinkedList.
+    struct LinkedList list;
+
+    /* PUBLIC MEMBER METHODS */
+    // The push function adds a node to the end of the chain.
+    void (*push)(struct Queue *queue, void *data, unsigned long size);
+    // The peek function retrieves the data from the front of the list.
+    void * (*peek)(struct Queue *queue);
+    // The pop function removes the first item in the list.
+    void (*pop)(struct Queue *queue);
+};
+
+
+
+// MARK: CONSTRUCTORS
+
+// The constructor should be used to create new Queue instances.
+struct Queue queue_constructor(void);
+// The destructor should be used to delete a Queue instance.
+void queue_destructor(struct Queue *queue);
+
+#endif /* Queue_h */
